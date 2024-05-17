a6452cd9f80c3e62ceb8c4051316163d7e4f11b7
José Manuel Cota <jmcota@ontica.org>
2024-05-17 11:04:35 -0600

    Empiria Payments Management System backend. Initial commit.

---
+++ Core.Tests/Helpers/TestingConstants.cs
@@ -0,0 +1,18 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Payments Core Module Tests                 Component : Tests Helpers                           *
+*  Assembly : Empiria.Payments.Core.Tests.dll            Pattern   : Testing constants                       *
+*  Type     : TestingConstants                           License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Provides testing constants for Empiria Payments Management System Core module.                 *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+namespace Empiria.Tests.Payments {
+
+  /// <summary>Provides testing constants for Empiria Payments Management System Core module.</summary>
+  static public class TestingConstants {
+
+  }  // class TestingConstants
+
+}  // namespace Empiria.Tests.Payments
---
+++ WebApi/Empiria.Payments.WebApi.AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  System   : Payments Management System                   Module  : Payments Web Api                        *
+*  Assembly : Empiria.Payments.WebApi.dll                  Pattern : Assembly Attributes File                *
+*                                                          License : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : HTTP/JSON web api used to interact with the Payments Management System's backend.              *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Reflection;
+
+/*************************************************************************************************************
+* Assembly configuration attributes.                                                                         *
+*************************************************************************************************************/
+[assembly: AssemblyTrademark("Empiria and Ontica are either trademarks of La Vía Óntica SC or Ontica LLC.")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
