b455c339e600723895874afb0dd3bd9f36db5b1e
Arturo Sosa <arturo.sosa@ontica.org>
2024-09-25 22:44:36 -0600

    Contract documents added

---
+++ Core/Contracts/Domain/ContractDocument.cs
@@ -0,0 +1,38 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Contracts Cucop Management                 Component : Domain Layer                            *
+*  Assembly : Empiria.Payments.Contracts.dll             Pattern   : Infomation Holder                       *
+*  Type     : ContractDocument                           License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Represents a contract document.                                                                *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+namespace Empiria.Payments.Contracts {
+
+  /// <summary>Represents a contract suppliers.</summary>
+  public class ContractDocument : GeneralObject {
+
+    #region Constructors and parsers
+
+    static internal ContractDocument Parse(int id) {
+      return BaseObject.ParseId<ContractDocument>(id);
+    }
+
+    static internal ContractDocument Parse(string uid) {
+      return BaseObject.ParseKey<ContractDocument>(uid);
+    }
+
+    static internal FixedList<ContractDocument> GetList() {
+      return BaseObject.GetList<ContractDocument>()
+                       .ToFixedList();
+    }
+
+    static internal ContractDocument Empty => BaseObject.ParseEmpty<ContractDocument>();
+
+    #endregion Constructors and parsers
+
+  }  // class ContractDocument
+
+}  // namespace Empiria.Payments.Contracts
