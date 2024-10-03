7453d1ac12608a07ef6fc1ab68cebacfe143e7e4
Arturo Sosa <arturo.sosa@ontica.org>
2024-10-03 13:02:42 -0600

    Added contract milestone and contract milestone items class

---
+++ Core/Contracts/Domain/ContractMilestone.cs
@@ -0,0 +1,38 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Contracts milestone Management             Component : Domain Layer                            *
+*  Assembly : Empiria.Payments.Contracts.dll             Pattern   : Infomation Holder                       *
+*  Type     : ContractMilestone                          License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Represents a contract milestone.                                                               *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+namespace Empiria.Payments.Contracts {
+
+  /// <summary>Represents a contract milesto.</summary>
+  public class ContractMilestone : GeneralObject {
+
+    #region Constructors and parsers
+
+    static internal ContractMilestone Parse(int id) {
+      return BaseObject.ParseId<ContractMilestone>(id);
+    }
+
+    static internal ContractMilestone Parse(string uid) {
+      return BaseObject.ParseKey<ContractMilestone>(uid);
+    }
+
+    static internal FixedList<ContractMilestone> GetList() {
+      return BaseObject.GetList<ContractMilestone>()
+                       .ToFixedList();
+    }
+
+    static internal ContractMilestone Empty => BaseObject.ParseEmpty<ContractMilestone>();
+
+    #endregion Constructors and parsers
+
+  }  // class ContractMilestone
+
+}  // namespace Empiria.Payments.Contracts
---
+++ Core/Contracts/Domain/ContractMilestoneItem .cs
@@ -0,0 +1,38 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Contracts milestone Management             Component : Domain Layer                            *
+*  Assembly : Empiria.Payments.Contracts.dll             Pattern   : Infomation Holder                       *
+*  Type     : ContractMilestoneItem                      License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Represents a contract milestone Item.                                                          *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+namespace Empiria.Payments.Contracts {
+
+  /// <summary>Represents a contract suppliers.</summary>
+  public class ContractMilestoneItem : GeneralObject {
+
+    #region Constructors and parsers
+
+    static internal ContractMilestoneItem Parse(int id) {
+      return BaseObject.ParseId<ContractMilestoneItem>(id);
+    }
+
+    static internal ContractMilestoneItem Parse(string uid) {
+      return BaseObject.ParseKey<ContractMilestoneItem>(uid);
+    }
+
+    static internal FixedList<ContractMilestoneItem> GetList() {
+      return BaseObject.GetList<ContractMilestoneItem>()
+                       .ToFixedList();
+    }
+
+    static internal ContractMilestoneItem Empty => BaseObject.ParseEmpty<ContractMilestoneItem>();
+
+    #endregion Constructors and parsers
+
+  }  // class ContractMilestone
+
+}  // namespace Empiria.Payments.Contracts
